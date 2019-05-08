1722b23d2a2e7e846b8ce98cecab9218c8fb6fe7
Mike Qureshi <mustafashakeel@gmail.com>
2019-05-07 19:08:52 -0700

    constants

---
+++ src/Constants.kt
@@ -0,0 +1,6 @@
+class Constants {
+    companion object{
+        const val COLOR="Red"
+
+    }
+}
--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,9 @@
+import Constants.Companion.COLOR
+
 fun main(args: Array<String>) {
+
+    println(" the Color is $COLOR")
+
+
 
 }
